d6b38c038b90f90682965e09f6bab97eded980a0
bohdev95 <boleinikov95@gmail.com>
2023-02-09 17:13:10 +0400

    Added tectonic transform, scale, rotate controls.
    Fixed position bug.

--- components/stlViewer/index.tsx
+++ components/stlViewer/index.tsx
@@ -2,7 +2,10 @@ import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { STLLoader as Loader } from "three/examples/jsm/loaders/STLLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TransformControls } from "three/examples/jsm/controls/TransformControls";
 import { createAnimate } from './stlHelpers/animate';
+import { centerGroup } from './stlHelpers/centerGroup';
+import { ArrowHelper, Vector3 } from 'three';
 
 const loader = new Loader();
 const textureLoader = new THREE.TextureLoader();
@@ -41,28 +44,35 @@ const wings = {
 	},
 }
 
-// will add core paces to model 
-const addCorePices = function (
+// will add core paces to model
+const addCorePieces = function (
 	intersect: THREE.Intersection<THREE.Object3D<THREE.Event>>,
 	scene: THREE.Scene, loader: Loader,
-	wing: any
+	wing: any,
+	transformControls: TransformControls,
 ) {
 	const coreModelPath = '/assets/tektonicCoreParts/CoreStep.stl'
 	const whiteTexture = '/assets/whiteTextureBasic.jpg'
-	
-	// render model 
+	let coreModelMesh = undefined;
+	let wingModelMesh = undefined;
+
+	const group = new THREE.Group();
+
+	// render model
 	loader.load(coreModelPath, (geometry) => {
 		const material = new THREE.MeshMatcapMaterial({
 			color: 0xffffff, // color for texture
 			matcap: textureLoader.load(whiteTexture)
 		});
-		const mesh = new THREE.Mesh(geometry, material);
-		mesh.geometry.computeVertexNormals();
-		mesh.geometry.center();
-		mesh.position.copy(intersect.point)
-		mesh.rotation.z = 1.65;  // will add some rotation
-		mesh.rotation.x = -0.3;   // rotate model of core element
-		scene.add(mesh);
+		coreModelMesh = new THREE.Mesh(geometry, material);
+		coreModelMesh.geometry.computeVertexNormals();
+		coreModelMesh.geometry.center();
+		coreModelMesh.position.copy(intersect.point)
+		coreModelMesh.rotation.z = 1.65;  // will add some rotation
+		coreModelMesh.rotation.x = -0.3;   // rotate model of core element
+		coreModelMesh.geometry.center();
+
+		group.attach(coreModelMesh);
 	});
 
 	// const wingPartPath = '/assets/tektonicWings/tectonic_long.stl'
@@ -71,44 +81,103 @@ const addCorePices = function (
 			color: 0xabdbe3, // color for texture
 			matcap: textureLoader.load(whiteTexture)
 		});
-		const mesh = new THREE.Mesh(geometry, material);
-		mesh.geometry.computeVertexNormals();
-		mesh.geometry.center();
-		mesh.position.copy(intersect.point)
+		wingModelMesh = new THREE.Mesh(geometry, material);
+		wingModelMesh.geometry.computeVertexNormals();
+		wingModelMesh.geometry.center();
+		wingModelMesh.position.copy(intersect.point)
 		// rotations
-		mesh.rotation.y = wing?.rotations.y;  // will add some rotation
-		mesh.rotation.x = wing?.rotations.x;   // rotate model of core element
+		wingModelMesh.rotation.y = wing?.rotations.y;  // will add some rotation
+		wingModelMesh.rotation.x = wing?.rotations.x;   // rotate model of core element
 
 		//possitions
 		if (wing?.movedPos.x)
-			mesh.position.x += wing?.movedPos.x
+			wingModelMesh.position.x += wing?.movedPos.x
 		if (wing?.movedPos.y)
-			mesh.position.y += wing?.movedPos.y
+			wingModelMesh.position.y += wing?.movedPos.y
 		if (wing?.movedPos.z)
-			mesh.position.z += wing?.movedPos.z
+			wingModelMesh.position.z += wing?.movedPos.z
 
 		// scales
-		mesh.scale.x = wing?.scale || 0.7
-		mesh.scale.y = wing?.scale || 0.7
-		mesh.scale.z = wing?.scale || 0.7
-		scene.add(mesh);
+		wingModelMesh.scale.x = wing?.scale || 0.7
+		wingModelMesh.scale.y = wing?.scale || 0.7
+		wingModelMesh.scale.z = wing?.scale || 0.7
+
+		group.attach(wingModelMesh);
+		centerGroup(group);
+		// const box = new THREE.BoxHelper(group, 0xffff00);
+		// scene.add(box);
 	});
 
+	scene.attach(group);
+	transformControls.detach();
+	transformControls.attach(group);
+	scene.attach(transformControls);
 }
 
 
 export default function StlViewer({ sizeX = 1500, sizeY = 1000, pathToModel = '/assets/Lyn.stl', pathToModelTexture = '/assets/whiteTextureBasic.jpg' }) {
 	const containerRef = useRef();
+	let transformControls = undefined;
+	let orbitControls = undefined;
+	let scene = undefined;
+	let camera = undefined;
+	let renderer = undefined;
+
 	useEffect(() => {
-		const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera(
+		scene = new THREE.Scene();
+		camera = new THREE.PerspectiveCamera(
 			750,
 			sizeX / sizeY,
 			10,
 			100000
 		);
-		var clickMouse = new THREE.Vector2();
-		// render model 
+		init();
+		renderModel();
+		//  three js window
+		if (containerRef.current)
+			(containerRef.current as any).appendChild(renderer.domElement);
+		const animate = createAnimate({ scene, camera, renderer });
+		camera.position.z = 350;
+		animate.animate();
+	});
+
+	const init = () => {
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize(sizeX, sizeY);
+		orbitControls = new OrbitControls(camera, renderer.domElement);
+		// add controls to window
+		// zoom parameters how much can zoom
+		orbitControls.maxDistance = 450;
+		orbitControls.minDistance = 125;
+		initTransformControls();
+
+		// const axesHelper = new THREE.AxesHelper( 100 );
+		// scene.add( axesHelper );
+	}
+
+	const initTransformControls = () => {
+		transformControls = new TransformControls(camera, renderer.domElement);
+		transformControls.addEventListener('change', () => {
+			renderer.render(scene, camera);
+		});
+		transformControls.addEventListener('dragging-changed', event => orbitControls.enabled = ! event.value);
+
+		window.addEventListener( 'keydown', event => {
+			switch ( event.keyCode ) {
+				case 87: // W
+					transformControls.setMode( 'translate' );
+					break;
+				case 69: // E
+					transformControls.setMode( 'rotate' );
+					break;
+				case 82: // R
+					transformControls.setMode( 'scale' );
+					break;
+			}
+		});
+	}
+
+	const renderModel = () => {
 		loader.load(pathToModel, (geometry) => {
 			const material = new THREE.MeshMatcapMaterial({
 				color: 0xffffff, // color for texture
@@ -118,42 +187,53 @@ export default function StlViewer({ sizeX = 1500, sizeY = 1000, pathToModel = '/
 			mesh.geometry.computeVertexNormals();
 			mesh.geometry.center();
 			// will add click method to object
-			renderer.domElement.addEventListener('click', (event) => {
-				clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-				clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-				//model event handler
-				const raycaster = new THREE.Raycaster();
-				raycaster.setFromCamera(clickMouse, camera);
-
-				// add and handle events
-				const intersects = raycaster.intersectObjects([mesh]);
-				if (intersects.length > 0) { // clicked on model or no
-					var intersect = intersects[0];
-					//show core screw/(implant) pices 
-					addCorePices(intersect, scene, loader, wings.wing5)
-				}
-			});
+			renderer.domElement.addEventListener('dblclick', (event) => handleDblClick(event, mesh));
+			// const box = new THREE.BoxHelper(mesh, 0xffff00);
+			// scene.add(box);
 			scene.add(mesh);
 		});
-		const renderer = new THREE.WebGLRenderer();
-		// add controls to window
-		const controls = new OrbitControls(camera, renderer.domElement);
-		// zoom parameters how much can zoom 
-		controls.maxDistance = 450;
-		controls.minDistance = 125;
-		const geometry = new THREE.BoxGeometry(0, 0, 0);
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // give materials or textures to model
-		const cube = new THREE.Mesh(geometry, material);
-		scene.add(cube);
-		//  three js window 
-		renderer.setSize(sizeX, sizeY);
-		if (containerRef.current)
-			(containerRef.current as any).appendChild(renderer.domElement);
-		const animate = createAnimate({ scene, camera, renderer });
-		camera.position.z = 350;
-		animate.animate();
-	}, []);
+	}
 
-	return <div ref={containerRef} />;
+	const handleDblClick = (event, mesh) => {
+		let clickMouse = new THREE.Vector2();
+
+		clickMouse.x = (event.clientX / sizeX) * 2 - 1;
+		clickMouse.y = -(event.clientY / sizeY) * 2 + 1;
+
+		const raycaster = new THREE.Raycaster();
+		raycaster.setFromCamera(clickMouse, camera);
+
+		// var arrow = new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 800, 0xff0000 );
+		// scene.add( arrow );
+
+		console.log(mesh);
+		const intersects = raycaster.intersectObjects([mesh]);
+		if (intersects.length > 0) { // clicked on model or no
+			let intersect = intersects[0];
+			//show core screw/(implant) pices
+			addCorePieces(intersect, scene, loader, wings.wing5, transformControls);
+		}
+	}
+
+	return (<>
+		<div id="info" style={{
+			position: "absolute",
+			top: "0px",
+			width: "100%",
+			padding: "10px",
+			boxSizing: "border-box",
+			textAlign: "center",
+			userSelect: "none",
+			pointerEvents: "none",
+			zIndex: 1,
+			color: '#ffffff'
+		}}>
+			"W" translate | "E" rotate | "R" scale | "+/-" adjust size<br />
+			"Q" toggle world/local space |  "Shift" snap to grid<br />
+			"X" toggle X | "Y" toggle Y | "Z" toggle Z | "Spacebar" toggle enabled<br />
+			"Esc" reset current transform<br />
+			"C" toggle camera | "V" random zoom
+		</div>
+		<div ref={containerRef} />
+	</>);
 }
